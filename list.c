2408f548362f5550afe75c40a596c79fd8463039
Aven <a@avenf.com>
2026-02-14 23:06:26 +0000

    moved List utility functions to a new file `list.c`

---
+++ list.c
@@ -0,0 +1,63 @@
+//
+// Created by aven on 14/02/2026.
+//
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/limits.h>
+
+typedef struct List {
+    char** list;
+    int size;
+    size_t capacity;
+} List;
+
+void addList(const char* string, List* list) {
+    char** temp = list->list;
+    if (list->capacity < list->size+2) {
+        temp = realloc(list->list, list->capacity*2 * sizeof(char*));
+        if (temp == NULL) {
+            perror("realloc failed");
+            return;
+        }
+        list->capacity = list->capacity*2;
+    }
+    list->list = temp;
+    list->list[list->size] = strdup(string);
+    if (list->list[list->size] == NULL) {
+        perror("strdup failed");
+        return;
+    }
+    list->list[list->size + 1] = NULL;
+    list->size++;
+}
+
+List splitString(char str[PATH_MAX], const char* delim) {
+    List returnList = {NULL, 0, 8};
+    returnList.list = malloc(sizeof(char*)*8);
+
+    if (returnList.list == NULL) {
+        perror("malloc failed");
+        return returnList;
+    }
+
+    const char* nextWord = strtok(str, delim);
+    while (nextWord != NULL) {
+        addList(nextWord, &returnList);
+        nextWord = strtok(NULL, delim);
+    }
+
+    return returnList;
+}
+
+void freeList(List *givenList) {
+    if (givenList->list != NULL) {
+        for (int i = 0; i < givenList->size; i++) {
+            free(givenList->list[i]);
+        }
+        free(givenList->list);
+        givenList->list = NULL;
+    }
+}
--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
-#include <time.h>
 #include <limits.h>
 #include <sys/wait.h>
 #include <errno.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include "list.c"
 
 #ifndef HOST_NAME_MAX
   #if defined(_POSIX_HOST_NAME_MAX)
@@ -37,12 +37,6 @@ void execute_command(char** args) {
     }
 }
 
-typedef struct List {
-    char** list;
-    int size;
-    size_t capacity;
-} List;
-
 bool startsWith(const char* string, const char* prefix) {
     const size_t slen = strlen(string);
     const size_t plen = strlen(prefix);
@@ -53,54 +47,6 @@ bool startsWith(const char* string, const char* prefix) {
         return false;
     }
     return strncmp(string, prefix, plen) == 0;
-}
-
-void addList(const char* string, List* list) {
-    char** temp = list->list;
-    if (list->capacity < list->size+2) {
-        temp = realloc(list->list, list->capacity*2 * sizeof(char*));
-        if (temp == NULL) {
-            perror("realloc failed");
-            return;
-        }
-        list->capacity = list->capacity*2;
-    }
-    list->list = temp;
-    list->list[list->size] = strdup(string);
-    if (list->list[list->size] == NULL) {
-        perror("strdup failed");
-        return;
-    }
-    list->list[list->size + 1] = NULL;
-    list->size++;
-}
-
-List splitString(char str[PATH_MAX], const char* delim) {
-    List returnList = {NULL, 0, 8};
-    returnList.list = malloc(sizeof(char*)*8);
-
-    if (returnList.list == NULL) {
-        perror("malloc failed");
-        return returnList;
-    }
-
-    const char* nextWord = strtok(str, delim);
-    while (nextWord != NULL) {
-        addList(nextWord, &returnList);
-        nextWord = strtok(NULL, delim);
-    }
-
-    return returnList;
-}
-
-void freeList(List *givenList) {
-    if (givenList->list != NULL) {
-        for (int i = 0; i < givenList->size; i++) {
-            free(givenList->list[i]);
-        }
-        free(givenList->list);
-        givenList->list = NULL;
-    }
 }
 
 typedef struct config {
